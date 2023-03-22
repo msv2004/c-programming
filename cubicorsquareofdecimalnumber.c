198c27cd9935a10b12ba47501e18240921dcb997
Shashe Vikaash <128576150+msv2004@users.noreply.github.com>
2023-03-22 13:56:43 +0530

    Create cubicorsquareofdecimalnumber.c

---
+++ cubicorsquareofdecimalnumber.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    float n;
+    printf("Enter the value of n:");
+    scanf("%f",&n);
+
+    printf("The Square of the given decimal value is %2f\n", n*n );
+    printf("The cube of the given decimal number is %2f\n", n*n*n);
+
+    return 0;
