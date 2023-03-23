b59833f927243f419ca474fba04a0f4784ed93db
Shashe Vikaash <128576150+msv2004@users.noreply.github.com>
2023-03-23 09:15:29 +0530

    Update TEST 4.3.c

--- TEST 4.3.c
+++ TEST 4.3.c
@@ -1,27 +1,71 @@
+3.Write a program to reverse an array
+Sample Input:
+Array of elements = {16, 18, 27, 16, 23, 21, 19}
+Sample Output:
+Reverse Array elements = {19, 21 23, 16, 27, 18, 16}
+Test cases:
+1.Array of elements = {26, 28, 37, 26, 33, 31, 29}
+2. Array of elements = {1.6, 1.8, 2.7, 1.6, 2.3, 2.1, .19}
+3. Array of elements = {0, 160, 180, 270, 160, 230, 210, 190, 0}
+4. Array of elements = {200, 180, 180, 270, 270, 270, 190, 200}
+5. Array of elements = {100, 100, 100, 100, 100, 100, 100, 100} 
+CODING:
 #include <stdio.h>
+#define MAX_SIZE 100
+int main()
+{
+    int arr[MAX_SIZE];
+    int i, n;
+    printf("Enter the size of the array: ");
 
-int n = sizeof(arr)/sizeof(arr[0]);
-    int temp;
+    scanf("%d", &n);    
+    printf("Enter %d elements in the array: ", n);
 
-    printf("Original Array: ");
-    for(int i=0; i<n; i++) {
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Original array: ");
+
+    for(i=0; i<n; i++)
+
+    {
         printf("%d ", arr[i]);
     }
+    printf("\nReversed array: ");
 
-    for(int i=0; i<n/2; i++) {
-        temp = arr[i];
-        arr[i] = arr[n-i-1];
-        arr[n-i-1] = temp;
-    }
+    for(i=n-1; i>=0; i--)
 
-    printf("\nReversed Array: ");
-    for(int i=0; i<n; i++) {
+    {
         printf("%d ", arr[i]);
     }
     return 0;
+
 }
 
-sample test case:
+OUTPUT:
 
-Original Array: 16 18 27 16 23 21 19 
-Reversed Array: 19 21 23 16 27 18 16 
+a.)Enter the size of the array: 7
+Enter 7 elements in the array: 26 28 37 26 33 31 29
+Original array: 26 28 37 26 33 31 29 
+Reversed array: 29 31 33 26 37 28 26 
+    
+b.)Enter the size of the array: 7
+Enter 7 elements in the array: 1.6 1.8 2.7 1.6 2.3 2.1 
+19Original array: 1 0 0 0 0 0 0 
+Reversed array: 0 0 0 0 0 0 1 
+    
+c.)Enter the size of the array: 9
+Enter 9 elements in the array:  0 160 180 270 160 230 210 19 
+Original array: 0 160 180 270 160 230 210 19 0 
+Reversed array: 0 19 210 230 160 270 180 160 0 
+    
+d.)Enter the size of the array: 8
+Enter 8 elements in the array: 200 180 180 270 270 270 190 200
+Original array: 200 180 180 270 270 270 190 200 
+Reversed array: 200 190 270 270 270 180 180 200 
+    
+e.)Enter the size of the array: 8
+Enter 8 elements in the array: 100 100 100 100 100 100 100 100
+Original array: 100 100 100 100 100 100 100 100 
+Reversed array: 100 100 100 100 100 100 100 100 
