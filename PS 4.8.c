3ed0e72052f20852d1df31ec005e9843ee18531f
Shashe Vikaash <128576150+msv2004@users.noreply.github.com>
2023-03-23 12:51:44 +0530

    Create PS 4.8.c

---
+++ PS 4.8.c
@@ -0,0 +1,18 @@
+predict the output or error(s) for the following program 
+
+code: msv
+
+#include <stdio.h>
+int main()
+{
+    char s[]="man";
+    int i;
+    for(i=0;s[i];i++) {
+    printf("\n%c%c%c%c",s[i],*(s+i),*(i+s),i[s]); }
+    return 0;
+}
+
+op:
+mmmm
+aaaa
+nnnn
