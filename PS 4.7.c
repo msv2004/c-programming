0abe21b88c536587a7ba8ae3f8414efda0da5e6f
Shashe Vikaash <128576150+msv2004@users.noreply.github.com>
2023-03-23 12:50:10 +0530

    Create PS 4.7.c

---
+++ PS 4.7.c
@@ -0,0 +1,13 @@
+predict the output or errors for the following program
+
+#include <stdio.h>
+int main()
+{
+    int i=5;
+    printf("%d%d%d%d%d%d",i++,i--,++i,--i,i);
+    return 0;
+}
+
+op
+455550
+orignal output should come 56655
