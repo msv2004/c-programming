63a0bac12f5f6bf3c96bb3c47ad1d7ea5e3a2a31
Shashe Vikaash <128576150+msv2004@users.noreply.github.com>
2023-03-24 14:51:02 +0530

    Update PS 5.20.c

--- PS 5.20.c
+++ PS 5.20.c
@@ -1,36 +1,19 @@
 Find out the error and show the output 
-#include<stdio.h>
 
-void swap(int* num1, int* num2);
+#include <stdio.h>
+#include
 
-int main()
+void main()
 {
-    int num1, num2;
-
-    printf("Enter two numbers: ");
-    scanf("%d %d", &num1, &num2);
-
-    printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
-
-    swap(&num1, &num2);
-
-    printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
-
-    return 0;
+    char str[50];
+    int i, length = 0;
+    clrscr();
+    printf("Enter the string: ");
+    gets(str);
+    for (i = 0; str[i] != '\0'; ++i) {
+        length++;
+    }
+    printf("Length of the string is %d", length);
+    return 1;
 }
-
-void swap(int* num1, int* num2)
-{
-    int temp;
-
-    temp = *num1;
-    *num1 = *num2;
-    *num2 = temp;
-}
-
-
-OUTPUT:
-
-Enter two numbers: 10 20
-Before swapping: num1 = 10, num2 = 20
-After swapping: num1 = 20, num2 = 10
+mistake vundi copy cheyakandi bro!
